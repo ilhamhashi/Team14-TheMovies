8051347abcc810928d3e6b0b9b0bf9bded6b2839
Khadra Khalif <kamk71385@edu.ucl.dk>
2025-08-21 12:54:26 +0200

    Tilføjet Cinemarepository

---
+++ TheMovies/MVVM/Model/Repositories/ICinemaRepository.cs
@@ -0,0 +1,12 @@
+﻿using TheMovies.MVVM.Model.Classes;
+
+namespace TheMovies.MVVM.Model.Repositories
+{
+    interface ICinemaRepository
+    {
+        IEnumerable<Cinema> GetAll();
+        /*void AddCinema(Cinema cinema);
+        void RemoveCinema(Cinema cinema);
+        void UpdateCinema(Cinema cinema);*/
+    }
+}
