05b69f97fed86c65af6f2e5ccc30759f1085d583
Naeg62306 <naeg62306@edu.ucl.dk>
2025-08-27 11:24:31 +0200

    Nye klasser

---
+++ TheMovies/MVVM/Model/Repositories/ICinemaHallRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMovies.MVVM.Model.Classes;
+
+namespace TheMovies.MVVM.Model.Repositories
+{
+    interface ICinemaHallRepository
+    {
+        IEnumerable<CinemaHall> GetAll();
+    }
+}
