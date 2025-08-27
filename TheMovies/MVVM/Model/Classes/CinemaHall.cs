439ec3fa639302a7f33b54f9e85ca60d444627dc
Naeg62306 <naeg62306@edu.ucl.dk>
2025-08-27 13:18:51 +0200

    Changes

--- TheMovies/MVVM/Model/Classes/CinemaHall.cs
+++ TheMovies/MVVM/Model/Classes/CinemaHall.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TheMovies.MVVM.Model.Classes
 {
-    class CinemaHall
+    public class CinemaHall
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
--- TheMovies/MVVM/Model/Classes/Customer.cs
+++ TheMovies/MVVM/Model/Classes/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TheMovies.MVVM.Model.Classes
 {
-    class Customer
+    public class Customer
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
